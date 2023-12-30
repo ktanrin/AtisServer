b803414470991e5ed4f0cd38cc9ebc3a44390f02
ktanrin <144651151+ktanrin@users.noreply.github.com>
2023-12-30 19:01:36 +0700

    add icon

--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,14 @@ module.exports = defineConfig({
   pluginOptions: {
     electronBuilder: {
       preload: 'src/preload.js',
-    }
-},
-  
+      builderOptions: {
+        win: {
+          icon: 'src/assets/icon.png'
+        },
+      }
+    },
+    
+    },
   configureWebpack: {
     // No need for target: 'electron-renderer' here, the Electron Vue CLI plugin should handle it
     resolve: {
